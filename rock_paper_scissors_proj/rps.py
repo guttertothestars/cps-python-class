cab33ff5c0fb545f81221e45e6e78c8d9bdf6cf9
guttertothestars <wwilkson@gmail.com>
2026-01-18 22:09:11 -0500

    added welcome message

--- rock_paper_scissors_proj/rps.py
+++ rock_paper_scissors_proj/rps.py
@@ -2,3 +2,8 @@
 import random
 import walkertools
 
+print(walkertools.create_banner("Rock! Paper! Scissors!", '*'))
+print_blank_lines(2)
+print("Do you need a refresher on the rules? Y/N")
+
+      
